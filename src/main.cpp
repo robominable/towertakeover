508513f06fecc976bdf43c4746d3a5a7ad76fa17
trylaarsdam <50586094+trylaarsdam@users.noreply.github.com>
2019-09-07 22:00:09 -0500

    Update main.cpp
    Drivetrain motor inits

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,10 @@ vex::competition Competition;
 vex::controller controller = vex::controller(vex::controllerType::primary);
 
 //motors
-
+vex::motor FLdrive = vex::motor(PORT1, vex::gearSetting::ratio18_1, false); //ports may need to be changed later based on dead ports
+vex::motor FRdrive = vex::motor(PORT2, vex::gearSetting::ratio18_1, true); //ports may need to be changed later based on dead ports
+vex::motor BLdrive = vex::motor(PORT3, vex::gearSetting::ratio18_1, false); //ports may need to be changed later based on dead ports
+vex::motor BRdrive = vex::motor(PORT4, vex::gearSetting::ratio18_1, true); //ports may need to be changed later based on dead ports 
 
 //sensors
 
