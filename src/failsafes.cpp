c8a7a771fa8ca4d77769da995f1085d1d05b4370
trylaarsdam <50586094+trylaarsdam@users.noreply.github.com>
2019-09-26 21:29:50 -0500

    Variable Task Created

--- src/failsafes.cpp
+++ src/failsafes.cpp
@@ -1,1 +1,6 @@
 //failsafe code goes here. would apply in all circumstances
+
+//variables for sanity. called continuously in task main of main.cpp
+void variable_set() {
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -51,10 +51,10 @@ int main() {
     
     //Run the pre-autonomous function. 
     pre_auton();
-       
+    variable_set();
     //Prevent main from exiting with an infinite loop.                        
     while(1) {
-      vex::task::sleep(100);//Sleep the task for a short amount of time to prevent wasted resources.
+      vex::task::sleep(75);//Sleep the task for a short amount of time to prevent wasted resources.
     }    
        
 }
