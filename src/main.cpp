56e612f3b9d35faf98f26960921a419fa13088e5
trylaarsdam <50586094+trylaarsdam@users.noreply.github.com>
2019-09-26 22:21:55 -0500

    Main Definitions

--- src/main.cpp
+++ src/main.cpp
@@ -21,14 +21,18 @@ vex::motor FLdrive = vex::motor(PORT1, vex::gearSetting::ratio18_1, false); //po
 vex::motor FRdrive = vex::motor(PORT2, vex::gearSetting::ratio18_1, true); //ports may need to be changed later based on dead ports
 vex::motor BLdrive = vex::motor(PORT3, vex::gearSetting::ratio18_1, false); //ports may need to be changed later based on dead ports
 vex::motor BRdrive = vex::motor(PORT4, vex::gearSetting::ratio18_1, true); //ports may need to be changed later based on dead ports 
+vex::motor DR4B = vex::motor(PORT5, vex::gearSetting::ratio36_1, false);
+vex::motor liftMotor = vex::motor(PORT6, vex::gearSetting::ratio36_1, false);
+vex::motor clawMotor = vex::motor(PORT7, vex::gearSetting::ratio18_1, false);
+vex::motor intakeMotor = vex::motor(PORT8, vex::gearSetting::ratio18_1, false);
+
 
 //sensors
 
 //jumpers
-
+//vex::digital_in brainDebug = vex::digital_in(vex::triport::A, vex::triportType::digitalInput); //idk wth is going on here, triport errors galore
 
 //variables
-
 //drive motors
 float FLtemp = 0;
 float FRtemp = 0;
@@ -39,15 +43,11 @@ float FRefficiency = 100;
 float BLefficiency = 100;
 float BRefficiency = 100;
 //DR4B motors
-float DR4BleftTemp = 0;
-float DR4BrightTemp = 0;
-float DR4BleftEfficiency = 100;
-float DR4BrightEfficiency = 100;
+float DR4Btemp = 0;
+float DR4Befficiency = 100;
 //2bar motors
-float left2barTemp = 0;
-float right2barTemp = 0;
-float left2barEfficiency = 100;
-float right2barEfficiency = 100;
+float liftTemp = 0;
+float liftEfficiency = 100;
 //claw motor
 float clawTemp = 0;
 float clawEfficiency = 100;
@@ -58,7 +58,27 @@ float intakeEfficiency = 100;
 //failsafe code goes here. would apply in all circumstances
 //variables for sanity. called continuously in task main of main.cpp
 void variable_set() {
-
+  //drive variables
+  FLtemp = FLdrive.temperature(percentUnits::pct);
+  FRtemp = FRdrive.temperature(percentUnits::pct);
+  BLtemp = BLdrive.temperature(percentUnits::pct);
+  BRtemp = BRdrive.temperature(percentUnits::pct);
+  FLefficiency = FLdrive.efficiency(percentUnits::pct);
+  FRefficiency = FRdrive.efficiency(percentUnits::pct);
+  BLefficiency = FLdrive.efficiency(percentUnits::pct);
+  BRefficiency = FRdrive.efficiency(percentUnits::pct);
+  //DR4B variables
+  DR4Btemp = DR4B.temperature(percentUnits::pct);
+  DR4Befficiency = DR4B.efficiency(percentUnits::pct);
+  //2bar variables
+  liftTemp = liftMotor.temperature(percentUnits::pct);
+  liftEfficiency = liftMotor.efficiency(percentUnits::pct);
+  //claw variables
+  clawTemp = clawMotor.temperature(percentUnits::pct);
+  clawEfficiency = clawMotor.efficiency(percentUnits::pct);
+  //intake variables
+  intakeTemp = intakeMotor.temperature(percentUnits::pct);
+  intakeEfficiency = intakeMotor.efficiency(percentUnits::pct);
 }
 
 void brainDebugMode() {
