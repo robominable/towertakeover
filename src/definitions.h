a66bb7e110bd5a23a6dceca9d23cd14d53cd9854
trylaarsdam <50586094+trylaarsdam@users.noreply.github.com>
2019-09-26 21:37:10 -0500

    Variable Creation
    and function creation/calling

--- src/definitions.h
+++ src/definitions.h
@@ -14,3 +14,32 @@ vex::motor BLdrive = vex::motor(PORT3, vex::gearSetting::ratio18_1, false); //po
 vex::motor BRdrive = vex::motor(PORT4, vex::gearSetting::ratio18_1, true); //ports may need to be changed later based on dead ports 
 
 //sensors
+
+
+//variables
+
+//drive motors
+float FLtemp = 0;
+float FRtemp = 0;
+float BLtemp = 0;
+float BRtemp = 0;
+float FLefficiency = 100;
+float FRefficiency = 100;
+float BLefficiency = 100;
+float BRefficiency = 100;
+//DR4B motors
+float DR4BleftTemp = 0;
+float DR4BrightTemp = 0;
+float DR4BleftEfficiency = 100;
+float DR4BrightEfficiency = 100;
+//2bar motors
+float left2barTemp = 0;
+float right2barTemp = 0;
+float left2barEfficiency = 100;
+float right2barEfficiency = 100;
+//claw motor
+float clawTemp = 0;
+float clawEfficiency = 100;
+//intake motor
+float intakeTemp = 0;
+float intakeEfficiency = 100;
--- src/failsafes.cpp
+++ src/failsafes.cpp
@@ -1,6 +1,10 @@
 //failsafe code goes here. would apply in all circumstances
-
+#include "definitions.h"
 //variables for sanity. called continuously in task main of main.cpp
 void variable_set() {
+  
+}
+
+void brainDebugMode() {
 
 }
